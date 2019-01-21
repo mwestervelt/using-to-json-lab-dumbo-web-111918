ef4ed38815511b4bfc0dd1e65dbf47674110d0e7
mwestervelt <elle.westervelt@gmail.com>
2019-01-21 16:40:58 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,9 +22,14 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
-  def show
-    @product = Product.find(params[:id])
-  end
+
+    def show
+       @product = Product.find(params[:id])
+       respond_to do |format|
+         format.html { render :show }
+         format.json { render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])}
+       end
+     end
 
   def data
     product = Product.find(params[:id])
